ec07d59e0061b350e52a028a47645b70d7453daf
Craig Sturzaker <craig.sturzaker@arup.com>
2017-04-03 20:54:09 +0100

    aysnc redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableSateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk'; //async library
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableSateInvariant())
+        applyMiddleware(thunk, reduxImmutableSateInvariant())
     );
 }
