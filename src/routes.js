52ea3b77be7bf2796415ff4aa57a82b144f0034d
Craig Sturzaker <craig.sturzaker@arup.com>
2017-04-03 21:45:19 +0100

    Manage course page

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 
@@ -9,6 +10,7 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="courses" component={CoursesPage}/>
+    <Route path="course/:id" component={ManageCoursePage}/>
     <Route path="about" component={AboutPage}/>
   </Route>
 );
